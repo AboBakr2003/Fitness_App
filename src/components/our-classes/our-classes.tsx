fedc95b1485e4e44a13a4a1a09c24e1a6adef71e
bekohaty <bekohaty@gmail.com>
2025-06-19 00:11:18 +0300

    add description of classes in our-classes section

--- src/components/our-classes/our-classes.tsx
+++ src/components/our-classes/our-classes.tsx
@@ -18,31 +18,35 @@ import { useState } from 'react';
 
 const classes: Array<ClassType> = [
   {
-    name: 'Weight Training Classes',
-    description: 'Lorem ipsum...',
+    name: "Weight Training Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     image: image1,
   },
   {
-    name: 'Yoga Classes',
+    name: "Yoga Classes",
     image: image2,
   },
   {
-    name: 'Ab Core Classes',
-    description: 'Lorem ipsum...',
+    name: "Ab Core Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     image: image3,
   },
   {
-    name: 'Adventure Classes',
-    description: 'Lorem ipsum...',
+    name: "Adventure Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     image: image4,
   },
   {
-    name: 'Fitness Classes',
+    name: "Fitness Classes",
     image: image5,
   },
   {
-    name: 'Training Classes',
-    description: 'Lorem ipsum...',
+    name: "Training Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     image: image6,
   },
 ];
